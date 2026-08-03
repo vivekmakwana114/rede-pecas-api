5ee4cae1c9c8611738470ade72bad42bbe2ae148
Vivek Makwana <vivek.makwana@invennico.com>
2026-08-03 15:17:58 +0530

    feat: postgre pool ssl mode when running in production

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,9 +5,10 @@ const { Pool } = pg;
 
 export const db = new Pool({
   connectionString: config.db.url,
+  ssl: config.env === 'production' && !config.db.url?.includes('localhost') ? { rejectUnauthorized: false } : false,
   max: 20,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 5000,
 });
 
 db.on('error', (err) => {
